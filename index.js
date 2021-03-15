1cdc95988fb4e27b4c8e37f111de99e9fc7cb6bd
Nguyễn Tuấn Duy <sylitas1999@icloud.com>
2021-03-15 16:25:38 +0700

    change port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pug = require("pug");
 const homepage = require("./models/hompage.router")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 //pug config
 app.set('view engine', 'pug');
 app.set("views","./views");
